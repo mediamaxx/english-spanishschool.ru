e62de3f6324840268bf7b97ed2723fb86ab6e9b6
AI-Alert <50011985+AI-Alert@users.noreply.github.com>
2021-08-18 14:16:40 +0200

    Small changes

--- template-parts/header/template.php
+++ template-parts/header/template.php
@@ -15,50 +15,76 @@ get_template_part('framework/template-parts/header/template');
 // Массив данных шапки сайта
 
 $arResult = [
-
-    'header_logo' => [
-        'full' => wp_get_attachment_image_url(carbon_get_theme_option( 'header_logo' ), 'full'),
-        'alt' => get_post_meta(carbon_get_theme_option( 'header_logo' ), '_wp_attachment_image_alt', TRUE),
-    ],
     'header_desc' => carbon_get_theme_option( 'header_desc' ),
     'header_phone' => carbon_get_theme_option( 'header_phone' ),
     'header_link_text' => carbon_get_theme_option( 'header_link_text' ),
     'header_link_text_phone' => carbon_get_theme_option( 'header_link_text_phone' ),
     'header_link' => carbon_get_theme_option( 'header_link' ),
 ];
+
+$image = [
+    'full' => wp_get_attachment_image_url(carbon_get_theme_option( 'header_logo' ), 'full'),
+    'alt' => get_post_meta(carbon_get_theme_option( 'header_logo' ), '_wp_attachment_image_alt', TRUE),
+];
 ?>
 
 <header class="page-header">
     <div class="container">
-        <?php if ( ($arResult['header_logo']['full']) || ($arResult['header_logo']['full'])):?>
-        <a href="/" class="page-header__logo">
-
-            <img src="$arResult['header_logo']['full']" alt="$arResult['header_logo']['alt']">
-        </a>
+        <?php if ( ($image['full']) || ($image['alt'])): ?>
+            <a href="/" class="page-header__logo">
+                <img src="<?= $image['full'] ?>" alt="<?= $image['alt'] ?>">
+            </a>
         <?php endif;?>
         <?php if ($arResult['header_desc']):?>
-        <div class="page-header__locality">
-            <svg>
-                <use xlink:href="./spritemap.svg#sprite-local"></use>
-            </svg>
-            <span><?= $arResult['header_desc'];?></span></div>
+            <div class="page-header__locality">
+                <svg>
+                    <use xlink:href="./spritemap.svg#sprite-local"></use>
+                </svg>
+                <span><?= $arResult['header_desc'];?></span></div>
         <?php endif; ?>
         <?php if ($arResult['header_phone'] || $arResult['header_link'] || $arResult['header_link_text'] || $arResult['header_link_text_phone']): ?>
-        <div class="page-header__contacts">
-            <a href="tel:<?= $arResult['header_phone'];?>" class="page-header__phone">
-                <span class="page-header__text"><?= $arResult['header_phone'];?></span>
-            </a>
-            <a href="tel:<?= $arResult['header_phone'];?>" class="page-header__phone page-header__phone--sm">
-                <span class="page-header__text"><?= $arResult['header_phone'];?></span>
-                <svg>
-                    <use xlink:href="./spritemap.svg#sprite-phone"></use>
-                </svg>
-            </a>
-            <a href="<?= $arResult['header_link'];?>" class="page-header__callback">
-                <span class="page-header__callback--xl"><?= $arResult['header_link_text'];?></span>
-                <span class="page-header__callback--md"><?= $arResult['header_link_text_phone'];?></span>
-            </a>
-        </div>
+            <div class="page-header__contacts">
+
+                <?php if($arResult['header_phone']): ?>
+                    <?php foreach ($arResult['header_phone'] as $item):
+                        $link = preg_replace( array( '/\s/', '/\(/', '/\)/', '/-/' ), '', $item['header_phone'] );
+                        $text = $item['header_phone'];
+                        ?>
+
+                        <a href="tel:<?= $link; ?>" class="page-header__phone">
+
+                            <span class="page-header__text"><?= $text; ?></span>
+                        </a>
+
+                    <?php endforeach; ?>
+                <?php endif; ?>
+
+
+                <?php if($arResult['header_phone']): ?>
+                    <?php foreach ($arResult['header_phone'] as $item):
+                        $link = preg_replace( array( '/\s/', '/\(/', '/\)/', '/-/' ), '', $item['header_phone'] );
+                        $text = $item['header_phone'];
+                        ?>
+
+                        <a href="tel:<?= $link; ?>" class="page-header__phone page-header__phone--sm">
+                            <span class="page-header__text"><?= $text; ?></span>
+                            <svg>
+                                <use xlink:href="./spritemap.svg#sprite-phone"></use>
+                            </svg>
+                        </a>
+
+                    <?php endforeach; ?>
+                <?php endif; ?>
+
+
+
+
+
+                <a href="<?= $arResult['header_link'];?>" class="page-header__callback">
+                    <span class="page-header__callback--xl"><?= $arResult['header_link_text'];?></span>
+                    <span class="page-header__callback--md"><?= $arResult['header_link_text_phone'];?></span>
+                </a>
+            </div>
         <?php endif; ?>
         <div class="page-header__burger" aria-label="Меню">
             <span class="page-header__burger-line"></span>
@@ -78,31 +104,31 @@ $arResult = [
         ?>
 
 
-<!--        <ul class="page-header__menu">-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Главная</a></li>-->
-<!--            <li class="page-header__menu-item menu-item menu-item-has-children">-->
-<!--                <a href="#" class="page-header__link">Английский язык</a>-->
-<!--                <ul class="sub-menu">-->
-<!--                    <li class="menu-item"><a href="#">Для детей</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Для взрослых</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Английский для туризма</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Корпоративное изучение</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Международные экзамены</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Профессиональные курсы английского языка</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Онлайн уроки</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Летние курсы</a></li>-->
-<!--                    <li class="menu-item"><a href="#">Индивидуальные занятия</a></li>-->
-<!--                </ul>-->
-<!--            </li>-->
-<!--            <li class="page-header__menu-item menu-item">-->
-<!--                <a href="#" class="page-header__link">Испанский язык</a>-->
-<!--            </li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">О школе</a></li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Отзывы</a></li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Цены</a></li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Блог</a></li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Акции</a></li>-->
-<!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Контакты</a></li>-->
-<!--        </ul>-->
+        <!--        <ul class="page-header__menu">-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Главная</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item menu-item-has-children">-->
+        <!--                <a href="#" class="page-header__link">Английский язык</a>-->
+        <!--                <ul class="sub-menu">-->
+        <!--                    <li class="menu-item"><a href="#">Для детей</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Для взрослых</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Английский для туризма</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Корпоративное изучение</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Международные экзамены</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Профессиональные курсы английского языка</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Онлайн уроки</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Летние курсы</a></li>-->
+        <!--                    <li class="menu-item"><a href="#">Индивидуальные занятия</a></li>-->
+        <!--                </ul>-->
+        <!--            </li>-->
+        <!--            <li class="page-header__menu-item menu-item">-->
+        <!--                <a href="#" class="page-header__link">Испанский язык</a>-->
+        <!--            </li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">О школе</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Отзывы</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Цены</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Блог</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Акции</a></li>-->
+        <!--            <li class="page-header__menu-item menu-item"><a href="#" class="page-header__link">Контакты</a></li>-->
+        <!--        </ul>-->
     </nav>
 </header>
