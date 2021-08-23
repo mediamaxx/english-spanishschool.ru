0ff08a73ed87061efcdf320c9c94878a9e073582
Johnyloh <50011985+AI-Alert@users.noreply.github.com>
2021-08-23 16:18:19 +0600

    Update services_promo.php
    Small changes

--- template-parts/services_promo.php
+++ template-parts/services_promo.php
@@ -14,18 +14,29 @@ $image = [
 
 
 ?>
-
+<?php if ($arResult['promo_title']||$arResult['promo_background_image']||$arResult['promo_subtitle']||$arResult['promo_text']||$arResult['promo_adv']):?>
 <section class="promo">
     <div class="container">
+        <?php if ($arResult['promo_title']):?>
         <div class="promo__title section-title"><?= $arResult['promo_title'] ?></div>
+        <?php endif;?>
+        <?php if ($arResult['promo_background_image']||$arResult['promo_subtitle']||$arResult['promo_text']||$arResult['promo_adv']):?>
         <div class="promo__wrap">
             <div class="promo__content">
+                <?php if ($arResult['promo_background_image']):?>
                 <div class="promo__cover">
                     <img src="<?= $image['full'] ?>" alt="<?= $arResult['promo_title'] ?>" class="promo__img alignright">
                 </div>
+                <?php endif;?>
+                <?php if ($arResult['promo_subtitle']):?>
                 <div class="promo__subtitle"><?= $arResult['promo_subtitle'] ?></div>
+                <?php endif;?>
+                <?php if ($arResult['promo_text']||$arResult['promo_adv']):?>
                 <div class="promo__text">
+                    <?php if ($arResult['promo_text']):?>
                     <p><?= $arResult['promo_text'] ?></p>
+                    <?php endif;?>
+                    <?php if ($arResult['promo_adv']):?>
                     <ul class="promo__list list-reset">
 
                         <?php foreach ($arResult['promo_adv'] as $item):
@@ -44,8 +55,12 @@ $image = [
 
 
                     </ul>
+                    <?php endif;?>
                 </div>
+                <?php endif;?>
             </div>
         </div>
+        <?php endif;?>
     </div>
 </section>
+<?php endif;?>
