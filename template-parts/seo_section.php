80dcaed8054ffc5ee87d9d2918495039fbbf0eb4
Johnyloh <50011985+AI-Alert@users.noreply.github.com>
2021-08-23 15:56:42 +0600

    Update seo_section.php
    Small changes

--- template-parts/seo_section.php
+++ template-parts/seo_section.php
@@ -7,12 +7,17 @@ $arResult = [
 
 ?>
 
-
+<?php if ($arResult['seo_title']||$arResult['seo_text']):?>
 <div class="seo">
     <div class="container">
+        <?php if ($arResult['seo_title']):?>
         <div class="seo__title section-title"><?= $arResult['seo_title']?></div>
+        <?php endif;?>
+        <?php if ($arResult['seo_text']):?>
         <div class="seo__content">
             <p><?= $arResult['seo_text']?></p>
         </div>
+        <?php endif;?>
     </div>
 </div>
+<?php endif;?>
