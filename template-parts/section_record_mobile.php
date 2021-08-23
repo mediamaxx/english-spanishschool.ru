a6b042ac0c6856b45a70d161f52832caa035b2b0
Johnyloh <50011985+AI-Alert@users.noreply.github.com>
2021-08-23 15:27:20 +0600

    Update section_record_mobile.php
    Small changes

--- template-parts/section_record_mobile.php
+++ template-parts/section_record_mobile.php
@@ -12,9 +12,15 @@ $arResult = [
 <div class="record record-mobile">
     <div class="container">
         <div class="record__container record-mobile__container">
+            <?php if ($arResult['form_title_mobile']):?>
             <div class="record-mobile__title"><?= $arResult['form_title_mobile']?></div>
+            <?php endif;?>
+            <?php if ($arResult['form_subtitle_mobile']):?>
             <div class="record-mobile__subtitle"><?= $arResult['form_subtitle_mobile']?></div>
+            <?php endif;?>
+            <?php if ($arResult['form_btn_text']||$arResult['form_btn_link']):?>
             <a href="<?= $arResult['form_btn_link']?>" class="record-mobile__link btn btn-secondary"><?= $arResult['form_btn_text']?></a>
+            <?php endif;?>
         </div>
     </div>
 </div>
